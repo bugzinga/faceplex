b1baba3e62de5f2517dbf37ddd68d8d36ad774bb
spollers <spollers@193a26f6-981e-4bb7-9254-53fcb655bdbf>
2012-05-13 17:36:16 +0000

    Faceplex ver. 1.0.45: Access to Pandora from outside the USA
    git-svn-id: svn+ssh://bugzinga.com/srv/repo-svn-csb@52 193a26f6-981e-4bb7-9254-53fcb655bdbf

--- code/background.js
+++ code/background.js
@@ -1,5 +1,33 @@
 
 /**
+ * Changing Proxy to get access to Pandora Radio outside the USA.
+ */
+$(document).ready(function($) {
+	jQuery.getScript('http://www.geoplugin.net/javascript.gp', function() {
+		if (geoplugin_countryCode() != "US") {
+			var config = {
+				mode: "pac_script",
+				pacScript: {
+				data: "function FindProxyForURL(url, host) {\n" +
+				      "  if (shExpMatch(url, 'http*://www.pandora.com/*')) {\n" + 
+				      "    return 'PROXY 50.22.104.219:80; PROXY 209.147.173.66:8080; PROXY 107.22.50.230:80; PROXY 107.20.164.163:80; PROXY 216.18.231.82:9090';\n" +
+				      "  }\n" +
+				      "  return 'DIRECT';\n" +
+				      "}"
+				}
+			};
+			chrome.proxy.settings.set(
+				{
+					value: config,
+					scope: 'regular'
+				},
+				function() {}
+			);	
+		}
+	});
+});
+
+/**
  * Chrome communication port name to communicate with Pandora's tab.
  */
 fpPandoraPortName = "Faceplex Pandora Port";
